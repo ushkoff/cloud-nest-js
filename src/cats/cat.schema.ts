a5501050d1c5483b2212f87e140cc687d7d4289f
ushkoff <lol454ushkbevise@gmail.com>
2023-12-07 14:19:22 +0200

    commit

--- src/cats/cat.schema.ts
+++ src/cats/cat.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-// Define the schema using a class
 @Schema()
 export class Cat {
   @Prop({ required: true })
@@ -14,8 +13,5 @@ export class Cat {
   breed: string;
 }
 
-// Create a Mongoose schema based on the Cat class
 export const CatSchema = SchemaFactory.createForClass(Cat);
-
-// Define the document interface
 export type CatDocument = Cat & Document;
